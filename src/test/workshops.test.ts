a4813c4af1d7ec460cfba1e0a15934c4fea4d4ec
Dmitry Lyfar <dmitry.lyfar@canonical.com>
2026-07-02 15:32:04 +1200

    Fix stale test assertions for OpenSSH key format and On icon color

--- src/test/workshops.test.ts
+++ src/test/workshops.test.ts
@@ -81,7 +81,7 @@ suite('reopenAction', () => {
 
 suite('statusIcon mapping', () => {
   const cases: Array<[Parameters<typeof statusIcon>[0], string, string | undefined]> = [
-    ['On', 'pass', 'charts.green'],
+    ['On', 'pass', 'disabledForeground'],
     ['Waiting', 'watch', 'charts.yellow'],
     ['Error', 'error', 'charts.red'],
     ['Off', 'circle-large-outline', 'disabledForeground'],
