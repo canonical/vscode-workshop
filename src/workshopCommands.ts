4144c89dc6babbf448f032128fb2bd7ed4532219
Dmitry Lyfar <dmitry.lyfar@canonical.com>
2026-08-13 10:37:13 +1200

    Keep comp check only at activation time as gate keeper

--- src/workshopCommands.ts
+++ src/workshopCommands.ts
@@ -21,7 +21,6 @@ import {
 import { LogsView } from './ui/logsView';
 import { createOpenPrompt } from './ui/openPrompt';
 import { WorkshopItem } from './ui/workshopsTree';
-import { assertWorkshopVersionCompatible } from './version';
 import {
   currentWorkshop,
   isWorkshopWindow,
@@ -139,8 +138,7 @@ export function createWorkshopCommands({
     }
     const localPath = folder.uri.fsPath;
 
-    void assertWorkshopVersionCompatible(client, log)
-      .then(() => client.ensureProject(localPath))
+    void client.ensureProject(localPath)
       .then(async (project) => {
         const pendingOp = readPendingOp(globalState, project.id);
         if (pendingOp) {
